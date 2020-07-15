7ae4bc848a32fe32644795682c6b58da2e020a9e
Frederick KOH <excubiteur@hotmail.com>
2020-07-15 15:08:50 +0800

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 gst-launch-1.0 uridecodebin \
-uri=file:///var/opt/xrvision/xr-edge/temp/videofiles/entranceNew2.mp4 caps='video/x-raw(memory:NVMM)' \
+uri=file:///home/user/videofiles/video.mp4 caps='video/x-raw(memory:NVMM)' \
 ! nvvideoconvert  \
 ! m.sink_0 nvstreammux name=m width=640 height=360 batch-size=1   \
 ! nvinfer config-file-path=./config_facenet_primary.txt   \
